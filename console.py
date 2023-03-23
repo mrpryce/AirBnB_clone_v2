9f8c1ca4c99e44b887315c5dc54495593fa97438
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-03-23 22:41:21 +0100

    Update console.py

--- console.py
+++ console.py
@@ -123,7 +123,8 @@ class HBNBCommand(cmd.Cmd):
                 arg_splited = arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
                 if type(arg_splited[1]) is str:
-                    arg_splited[1] = arg_splited[1].replace("_", " ")
+                    arg_splited[1] = arg_splited[1]
+                    .replace("_", " ")
                     .replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
@@ -321,6 +322,7 @@ class HBNBCommand(cmd.Cmd):
 
         new_dict.save()  # save updates to file
 
+    
     def help_update(self):
         """ Help information for the update class """
         print("Updates an object with new information")
